3551b7e23c78d2480b94c855da1747a82aac18c1
Bauti07 <bautistaschneider777@gmail.com>
2022-09-07 16:24:01 -0300

    creando la ruta para que el admin pueda eliminar un usuario y una para que los usuarios puedan actualizar sus datos

--- Back/routes/user.js
+++ Back/routes/user.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const { validateAuth, validateAdmin } = require("../middleware/auth");
 
-const {register, login, me, all} = require("../controllers/user");
+const {register, login, me, all, deleteUser, updateUser, } = require("../controllers/user");
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", validateAuth, me);
 router.get("/all", validateAuth, validateAdmin, all);
+router.delete("/:id", validateAuth, validateAdmin, deleteUser );
+router.put("/:id", updateUser);
 
 module.exports = router

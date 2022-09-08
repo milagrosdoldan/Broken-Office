3824d330fc9c24d140f9a4a8b525e59963c75a44
Franco Jalil <francojalil7@gmail.com>
2022-09-08 18:30:47 -0300

    falta funcionalidad a modal para editar perfil

---
+++ Front/src/pages/NotFound.jsx
@@ -0,0 +1,27 @@
+import { Box, Heading, Image, Text } from "@chakra-ui/react";
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alingItems="center"
+        alignItems="center"
+      >
+        <Heading fontSize="20vh">404!</Heading>
+        <Text mb="20" mx="10">
+          Lo sentimos, no se pudo encontrar la página que buscas.
+        </Text>
+        <Image
+          objectFit="cover"
+          src="https://statics.globant.com/production/public/2021-01/404%20desktop.png"
+          alt="Error 404"
+        />
+      </Box>
+    </>
+  );
+};
+
+export default NotFound;

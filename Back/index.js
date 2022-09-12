ef87a16d8478f744e1d57a6128a0ad2e5654626e
milagrosdoldan <milagrosivanadoldan@gmail.com>
2022-09-12 16:30:29 -0300

    inteligencia artificial funcionando

--- Back/index.js
+++ Back/index.js
@@ -12,12 +12,14 @@ require("dotenv").config();
 //middelware
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "4mb", extended: true }));
+app.use(
+  express.urlencoded({ limit: "4mb", extended: true, parameterLimit: 50000 })
+);
 app.use(morgan("tiny"));
 app.use(cookieparser());
-app.use(express.urlencoded({ extended: true }));
- app.use("/api", routes);
 
+app.use("/api", routes);
 
 app.listen(process.env.PORT, () => {
   console.log("api working!...");
